728ebdee44fd97e3e5627e21fba0515a89b1d457
MD Jawad <122153153+Mo-Jawad@users.noreply.github.com>
2026-09-24 10:56:30 +0600

    in singly LList, we added insert on the begin and middle feature

--- Singly-Linked-List/singlyLList.py
+++ Singly-Linked-List/singlyLList.py
@@ -9,6 +9,38 @@ class SinglyLL:
     def __init__(self):
         self.head = None
 
+    def insertAtBegin(self, data):
+        newNode = Node(data)
+
+        if self.head is None:
+            self.head = newNode
+            return 
+        else:
+            newNode.next = self.head 
+            self.head = newNode
+            return 
+        
+
+
+    def insertInMiddle(self, data, num):
+        newNode = Node(data)
+
+        if self.head is None:
+            print("List is empty")
+            return 
+
+        current = self.head 
+
+        while current is not None:
+            if current.data == num:
+                newNode.next = current.next 
+                current.next = newNode
+                return
+
+            current = current.next
+
+        print("enterred false number")
+
     def append(self, data):
         new_node = Node(data)
 
@@ -30,6 +62,11 @@ LList = SinglyLL()
 LList.append(30)
 LList.append(20)
 LList.append(60)
+
+LList.insertInMiddle(80, 60)
+LList.insertInMiddle(820, 20)
+
+LList.insertAtBegin(700)
 
 current = LList.head
 
